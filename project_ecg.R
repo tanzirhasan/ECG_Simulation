c286d9440df74928b82849bdec40dc95d9a26260
Luyan Yao <luyanyao@Luyans-MacBook.lan>
2017-02-24 23:52:11 -0800

    complete ECG wave

--- project_ecg.R
+++ project_ecg.R
@@ -1,15 +1,10 @@
-
-# transform  matlab code to R code to simulate P wave.This is the first step. Then other waves (qrs,t) could be simulated and added together 
-# to create a complete ECG wave.It is more flexible, since we could set and control some important variables of ECG wave, such as heart beat,
-#wave peak,interval, duration. 
-
-# variables: 2l=heartbeat/60s, ampitude = a_pwav, b=(2*l)/d_pwave, =x+t_pwav(p_r interval)
+# variables: 2l=60s/heart beat, ampitude = a_pwav, b=(2*l)/d_pwave, =x+t_pwav(p_r interval)
 
 #### step1: p wave
 p_wave<-function(x)
-{ l=1# half heartbeat cycle.eg.60 heartbeat/60sec=1 cycle
+{ l=1# half heartbeat cycle.eg.s/heartbeat=1 cycle
 a=0.25#amplitude of p wave
-x=x+(1/1.8)# x is the wave starting point shifted .
+x=x+(1/1.8)# x is the wave starting point shifted.
 b=3# 2l/b is duration of p wave.
 n=100 # fourier series levels, the bigher the more accurate
 p1=1/l # baseline
@@ -34,51 +29,75 @@ q_wave<-function (x){
   l=1
   x=x+1/6
   a=a_qwav =0.025# amplitude 
-   b=16#duration
+  b=16#duration
   n=100
-   q1=(a/(2*b))*(2-b)
-   q2=0
-for (i in 1:n){
-harm5=(((2*b*a)/(i*i*pi*pi))*(1-cos((i*pi)/b)))*cos((i*pi*x)/l);
-q2=q2+harm5}
-qwav=-1*(q1+q2)
-return (qwav)
+  q1=(a/(2*b))*(2-b)
+  q2=0
+  for (i in 1:n){
+    harm5=(((2*b*a)/(i*i*pi*pi))*(1-cos((i*pi)/b)))*cos((i*pi*x)/l);
+    q2=q2+harm5}
+  qwav=-1*(q1+q2)
+  return (qwav)
 }
-   x<-seq(0,5,0.001) 
-   qwav<-q_wave(x)
-   plot(x=x, y=qwav, type='l', xlim=c(0,5), ylim=c(0, 1), xlab="ECG wave", ylab="MV",main="ECG q wave simulation")
+x<-seq(0,5,0.001) 
+qwav<-q_wave(x)
+plot(x=x, y=qwav, type='l', xlim=c(0,5), ylim=c(0, 1), xlab="ECG wave", ylab="MV",main="ECG q wave simulation")
 
-  ####qrs wave
+####qrs wave
 qrs_wave<-function(x){
-l=1
-a=1
-b=5
-n=100
-qrs1=(a/(2*b))*(2-b)
-qrs2=0
-for (i in 1:n){
-harm=(((2*b*a)/(i*i*pi*pi))*(1-cos((i*pi)/b)))*cos((i*pi*x)/l);
-qrs2=qrs2+harm}
-qrswav=qrs1+qrs2
-return(qrswav)
+  l=1
+  a=1
+  b=5
+  n=100
+  qrs1=(a/(2*b))*(2-b)
+  qrs2=0
+  for (i in 1:n){
+    harm=(((2*b*a)/(i*i*pi*pi))*(1-cos((i*pi)/b)))*cos((i*pi*x)/l);
+    qrs2=qrs2+harm}
+  qrswav=qrs1+qrs2
+  return(qrswav)
 }
 x<-seq(0,5,0.001) 
 qrswav<-qrs_wave(x)
 plot(x=x, y=qrswav, type='l', xlim=c(0,5), ylim=c(0, 1), xlab="ECG wave", ylab="MV",main="ECG qrs wave simulation")
 
-#####s wave
+##### s wave
 s_wave<-function(x){
-l=1;
-x=x-1/6
-a=0.25
-b=15
-n=100;
-s1=(a/(2*b))*(2-b);
-s2=0;
-for (i in 1:n){
-harm3=(((2*b*a)/(i*i*pi*pi))*(1-cos((i*pi)/b)))*cos((i*pi*x)/l);
-s2=s2+harm3}
-swav=-1*(s1+s2)
-return(swav)}
+  l=1
+  x=x-1/6
+  a=0.25
+  b=15
+  n=100;
+  s1=(a/(2*b))*(2-b);
+  s2=0;
+  for (i in 1:n){
+    harm3=(((2*b*a)/(i*i*pi*pi))*(1-cos((i*pi)/b)))*cos((i*pi*x)/l);
+    s2=s2+harm3}
+  swav=-1*(s1+s2)
+  return(swav)}
+x<-seq(0,2,0.001) 
 swav<-s_wave(x)
 plot(x=x, y=swav, type='l', xlim=c(0,5), ylim=c(0, 1), xlab="ECG wave", ylab="MV",main="ECG s wave simulation")
+
+##### t wave
+
+t_wave<-function(x){
+  l=1
+  a=0.35
+  x=x-1/1.8
+  b=7
+  n=20
+  t1=1/l
+  t2=0
+  for (i in 1:n){
+    harm2=(((sin((pi/(2*b))*(b-(2*i))))/(b-(2*i))+(sin((pi/(2*b))*(b+(2*i))))/(b+(2*i)))*(2/pi))*cos((i*pi*x)/l);             
+    t2=t2+harm2}
+  twav1=t1+t2
+  twav=0.35*twav1
+  return (twav)}
+x<-seq(0,5,0.001) 
+twav<-t_wave(x)
+plot(x=x, y=twav, type='l', xlim=c(0,5), ylim=c(0, 1), xlab="ECG wave", ylab="MV",main="ECG t wave simulation")
+###### adding waves
+ecg<-pwav+qrswav+twav+swav+qwav
+plot(x,ecg,xlab = 'x',ylab='Mv',main='ECG wave')
